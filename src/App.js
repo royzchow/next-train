cd173960653823735a9d325ee70f6be8b7813a25
royzchow <roychow123@gmail.com>
2023-12-12 00:29:27 +0800

    minor bug fix
    minor bug fix

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ function App() {
   const [post, setPost] = useState(null);
   const [time, setTime] = useState(null);
 
-  useEffect(() => {
+  const getData = () => {
     axios.get(baseURL).then((response) => {
       setPost(response.data);
       setTime(Date.parse(response.data.data["TKL-LHP"].DOWN[0].time) - new Date())
       // setPost(JSON.parse('{"status":1,"message":"successful","sys_time":"2023-12-11 0:44:56","curr_time":"2023-12-11 00:44:56","data":{"TKL-LHP":{"curr_time":"2023-12-11 00:44:56","sys_time":"2023-12-11 0:44:56","DOWN":[{"ttnt":"4","valid":"Y","plat":"2","time":"2023-12-11 00:48:00","source":"-","dest":"TIK","seq":"1"}]}},"isdelay":"N"}'))
     });
+  }
+
+  useEffect(() => {
+    getData();
   }, []);
+
+  useEffect(() => {
+    time <= 0 && getData();
+  }, [time])
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -24,13 +32,13 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(t => t - 1000)
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-
+    if(post){
+      const interval = setInterval(() => {
+        setTime(Date.parse(post.data["TKL-LHP"].DOWN[0].time) - new Date())
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [post]);
 
   return (
     <center>
@@ -38,7 +46,11 @@ function App() {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        height: "100vh", width: "100vw", backgroundImage: "linear-gradient(to bottom, #294666, #172339)"
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0, width: "100vw", backgroundImage: "linear-gradient(to bottom, #294666, #172339)"
       }}>
         <div style={{ alignItems: 'center', justifyContent: 'center' }}>
           <div style={{ alignItems: 'center', justifyContent: 'center', fontSize: 100 }}>
